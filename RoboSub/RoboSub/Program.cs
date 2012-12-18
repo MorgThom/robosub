00a9e2f53379a4fedde7b3bedd6bbcf0fda9a583
Minh Nguyen <minhnh.91@gmail.com>
2012-12-17 23:21:34 -0700

    I commited the movement files to the project. When you have time check and see if they work.

--- RoboSub/RoboSub/Program.cs
+++ RoboSub/RoboSub/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Pololu.Jrk;
 
 namespace RoboSub
 {
@@ -18,12 +19,34 @@ namespace RoboSub
 
             Console.WriteLine("RoboSub Console Output");
 
+            Jrk jrk = null;
+            var deviceList = Jrk.getConnectedDevices();
+            if (deviceList.Count > 0)
+            {
+                Console.WriteLine("Connected to USB port " + deviceList[0].serialNumber);
+                jrk = new Jrk(deviceList[0]);
+            }
+            else
+            {
+                Console.WriteLine("Can't find any motor");
+            }
+            
             while (true)
             {
                 Console.SetCursorPosition(0, 2);
                 Console.WriteLine("Depth: {0:0.00} feet  ", pSensor.getDepth());
                 Console.WriteLine("Pressure: {0:0.00} psi  ", pSensor.Psi);
-
+                if (deviceList.Count > 0)
+                {
+                    if (pSensor.getDepth() > 0.4)
+                    {
+                        jrk.setTarget(2548);
+                    }
+                    else
+                    {
+                        jrk.setTarget(2048);
+                    }
+                }
             }
             
             
